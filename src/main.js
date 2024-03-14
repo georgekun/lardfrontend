86813c0af7596205b407a18456e55407941af3ff
jordan <georgeknyazyan@MacBook-Air-Dzordz.local>
2024-03-14 16:11:19 +0300

    added file with custom styles

--- src/main.js
+++ src/main.js
@@ -4,15 +4,18 @@ import store from '@/store'
 import router from "@/router";
 
 import 'vuetify/styles'
+import 'vuetify/dist/vuetify.min.css'
+
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+
+import "@/assets/styles/main.css"
 
 const vuetify= createVuetify({
     components,
     directives,
 })
-
 
 const app = createApp(App)
 
