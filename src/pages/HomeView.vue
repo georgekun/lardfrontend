c9edab4419ab3d26f4d12356589f3fac334bf3f1
jordan <georgeknyazyan@MacBook-Air-Dzordz.local>
2024-03-14 16:12:32 +0300

    добавлен main компонент + настроена сетка

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,13 +1,29 @@
 <script setup>
+import DrawerRight from "@/components/DrawerRight.vue";
+import MainContent from "@/components/MainContent.vue";
 
 </script>
 
 <template>
   <div class="home">
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col cols="8">
+          <MainContent></MainContent>
+        </v-col>
+        <v-col cols="4">
+          <drawer-right></drawer-right>
+        </v-col>
+      </v-row>
+    </v-container>
 
   </div>
 </template>
 
 <style scoped>
-
+  .home{
+    padding:20px 20px;
+    background-color: #F7F8F9;
+    min-height:100vh;
+  }
 </style>
