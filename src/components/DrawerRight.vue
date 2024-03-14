613b339e51d554a2778daf80e6b4bf646a445f3c
jordan <georgeknyazyan@MacBook-Air-Dzordz.local>
2024-03-14 16:11:58 +0300

    new components

---
+++ src/components/DrawerRight.vue
@@ -0,0 +1,89 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="drawer">
+      <v-container class="padding-20 border-bottom">
+          <v-btn
+              class="text-none"
+              prepend-icon=""
+              width="100%"
+              color="blue"
+              size="x-large"
+              text="Добавить сотрудника">
+          </v-btn>
+      </v-container>
+
+      <v-container class="padding-20">
+          <v-row class="">
+            <v-col>
+              <h1 class="">Фильтр</h1>
+            </v-col>
+          </v-row>
+
+          <v-row class="">
+            <v-col>
+              <p>Гражданство</p>
+              <v-select
+                  id="select1"
+                  label="Select"
+                  density="compact"
+                  :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+                  variant="solo-filled"
+              ></v-select>
+            </v-col>
+
+            <v-col>
+              <p>Пол</p>
+              <v-select
+                  id="select1"
+                  label="Select"
+                  density="compact"
+                  :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+                  variant="solo-filled"
+              ></v-select>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>
+              <p>Должность</p>
+              <v-select
+                  id="select1"
+                  label="Select"
+                  density="compact"
+                  :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+                  variant="solo-filled"
+              ></v-select>
+            </v-col>
+          </v-row>
+
+          <v-row class="border-bottom">
+            <v-col>
+              <p>Тип договора</p>
+
+              <v-checkbox
+                  label="John"
+                  value="John"
+              ></v-checkbox>
+              <v-checkbox
+                  label="Jacob"
+                  value="Jacob"
+              ></v-checkbox>
+
+            </v-col>
+          </v-row>
+
+        <v-row justify="center">
+          <v-col><v-btn width="100%" class="text-none" color="green">Применить</v-btn></v-col>
+          <v-col><v-btn width="100%" class="text-none" color="grey">Очистить</v-btn></v-col>
+        </v-row>
+      </v-container>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,54 @@
+<script setup>
+const filterButtons = [
+  {id:0, color:"#B0BCC7", label:"Весь список"},
+  {id:1, color:"#E2BD06", label:"Проблемные"},
+  {id:2, color:"#E52E2E", label:"Критические"},
+  {id:3, color:"#00B6ED", label:"Есть замечания"},
+  {id:4, color:"#00AE5B", label:"Выполнено"},
+]
+</script>
+
+<template>
+  <div class="main-content drawer">
+    <v-container class="padding-20 border-bottom ">
+      <v-row>
+        <v-col>
+          <v-text-field placeholder="Поиск сотрудника"
+                        density="compact" :disabled="false"
+                        :loading="false"></v-text-field>
+
+          <p class="text-grey-lighten-1">Например: Иван Иванов</p>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container class="padding-20">
+        <v-row>
+          <v-col>
+            <h1>Список сотрудников</h1>
+          </v-col>
+        </v-row>
+        <v-row justify="start">
+            <v-col>
+              <div class="button-group flex">
+                <v-btn
+                    class="text-none" :color="filterButtons[0].color"
+                    rounded="xl">
+                  <span class="text-white">{{ filterButtons[0].label }}</span>
+                </v-btn>
+                <div v-for="btn in filterButtons.slice(1)" :key="btn.id">
+                  <v-btn
+                      class="text-none" :color="btn.color" rounded="xl" variant="tonal">
+                    {{ btn.label }}
+                  </v-btn>
+                </div>
+              </div>
+            </v-col>
+        </v-row>
+    </v-container>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
